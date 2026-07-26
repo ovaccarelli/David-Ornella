7fef35628aa3f75ee3f6b5d450d80a5631190b29
ovaccarelli <o.vaccarelli@gmail.com>
2026-07-26 03:16:20 +0200

    fix: organize contacts by person

--- _includes/layouts/Footer.tsx
+++ _includes/layouts/Footer.tsx
@@ -13,15 +13,20 @@ export function Footer() {
       <div className="footerContacts">
         <h2>{t.footer.contacts}</h2>
         <address>
-          <div>
+          <div className="contactHead" aria-hidden="true">
+            <span />
             <span>{t.footer.email}</span>
-            <a href="mailto:o.vaccarelli@gmail.com">o.vaccarelli@gmail.com</a>
-            <a href="mailto:david.sc.38@gmail.com">david.sc.38@gmail.com</a>
-          </div>
-          <div>
             <span>{t.footer.phone}</span>
+          </div>
+          <div className="contactPerson">
+            <strong>Ornella</strong>
+            <a href="mailto:o.vaccarelli@gmail.com">o.vaccarelli@gmail.com</a>
             <a href="tel:+33768423177">+33768423177</a>
-            <a href="tel:+3365084371">+3365084371</a>
+          </div>
+          <div className="contactPerson">
+            <strong>David</strong>
+            <a href="mailto:david.sc.38@gmail.com">david.sc.38@gmail.com</a>
+            <a href="tel:+33665084371">+33665084371</a>
           </div>
         </address>
       </div>
