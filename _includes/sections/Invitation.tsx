af1cb5df559ac0dcac8e25a0fb768d38c56c2c37
ovaccarelli <o.vaccarelli@gmail.com>
2026-07-26 21:37:15 +0200

    fix: restore fourth photo and refine frames

--- _includes/sections/Invitation.tsx
+++ _includes/sections/Invitation.tsx
@@ -10,7 +10,6 @@ export function Invitation() {
       <div className="invitationCollage">
         <div className="photoPair photoPairLeft" aria-hidden="true">
           <figure><img src="assets/images/1.jpg" alt="" /></figure>
-          <span className="goldenThread" />
           <figure><img src="assets/images/2.jpg" alt="" /></figure>
         </div>
         <div className="invitationCopy">
@@ -21,8 +20,7 @@ export function Invitation() {
         </div>
         <div className="photoPair photoPairRight" aria-hidden="true">
           <figure><img src="assets/images/3.jpg" alt="" /></figure>
-          <span className="goldenThread" />
-          <figure><img src="assets/images/4.jpg" alt="" /></figure>
+          <figure><img src="assets/images/4.png" alt="" /></figure>
         </div>
       </div>
     </section>
