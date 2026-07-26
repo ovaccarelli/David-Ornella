ee5ecd02d53488d85cd16f7cc017dff1eb760e01
ovaccarelli <o.vaccarelli@gmail.com>
2026-07-26 03:14:16 +0200

    feat: add multilingual footer contacts

--- _includes/layouts/Footer.tsx
+++ _includes/layouts/Footer.tsx
@@ -10,6 +10,21 @@ export function Footer() {
       <p>{t.date} · Masseria Papaperta</p>
       <p className="footerLine">{t.footer.line}</p>
       <a className="giftLink" href="regalo/">{t.footer.gift}</a>
+      <div className="footerContacts">
+        <h2>{t.footer.contacts}</h2>
+        <address>
+          <div>
+            <span>{t.footer.email}</span>
+            <a href="mailto:o.vaccarelli@gmail.com">o.vaccarelli@gmail.com</a>
+            <a href="mailto:david.sc.38@gmail.com">david.sc.38@gmail.com</a>
+          </div>
+          <div>
+            <span>{t.footer.phone}</span>
+            <a href="tel:+33768423177">+33768423177</a>
+            <a href="tel:+3365084371">+3365084371</a>
+          </div>
+        </address>
+      </div>
     </footer>
   );
 }
